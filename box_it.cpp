0872a56d39b3d76b1441d9dbf9d2704c2ea00e44
EgorFollia <egorfollia@gmail.com>
2019-01-19 17:22:58 +0300

    Congrats! You have earned your 4th star.

---
+++ box_it.cpp
@@ -0,0 +1,109 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Box
+{
+private:
+  int l, b, h;
+
+public:
+  Box()
+  {
+    l = 0, b = 0, h = 0;
+  }
+
+  Box(int length, int breadth, int height)
+  {
+    l = length;
+    b = breadth;
+    h = height;
+  }
+
+  Box(Box& B)
+  {
+    l = B.getLength();
+    b = B.getBreadth();
+    h = B.getHeight();
+  }
+
+  int getLength() { return l; }
+  int getBreadth() { return b; }
+  int getHeight() { return h; }
+
+  long long CalculateVolume() { return (long long)(l * b * h); }
+
+  bool operator<(const Box &B)
+  {
+    int ll = B.getLength();
+    int bb = B.getBreadth();
+    int hh = B.getHeight();
+
+    if(l < ll || (b < bb && l==ll) || (h < hh && b==bb && l==ll))
+      return true;
+    else
+      return false;
+  }
+
+  ostream& operator<<(ostream& out, Box B)
+  {
+    int l = B.getLength();
+    int b = B.getBreadth();
+    int h = B.getHeight();
+
+    return out << l << ' ' << b << ' ' << h;
+  }
+};
+
+void check2()
+{
+	int n;
+	cin>>n;
+	Box temp;
+	for(int i=0;i<n;i++)
+	{
+		int type;
+		cin>>type;
+		if(type ==1)
+		{
+			cout<<temp<<endl;
+		}
+		if(type == 2)
+		{
+			int l,b,h;
+			cin>>l>>b>>h;
+			Box NewBox(l,b,h);
+			temp=NewBox;
+			cout<<temp<<endl;
+		}
+		if(type==3)
+		{
+			int l,b,h;
+			cin>>l>>b>>h;
+			Box NewBox(l,b,h);
+			if(NewBox<temp)
+			{
+				cout<<"Lesser\n";
+			}
+			else
+			{
+				cout<<"Greater\n";
+			}
+		}
+		if(type==4)
+		{
+			cout<<temp.CalculateVolume()<<endl;
+		}
+		if(type==5)
+		{
+			Box NewBox(temp);
+			cout<<NewBox<<endl;
+		}
+
+	}
+}
+
+int main()
+{
+	check2();
+}
